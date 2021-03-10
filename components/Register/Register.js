ad3801b115fe09c4045b715cfe4aa218e7ff4187
Jaskaran Singh <jaskaran1523@gmail.com>
2021-03-09 22:45:56 -0800

    Create register page

--- components/Register/Register.js
+++ components/Register/Register.js
@@ -20,25 +20,28 @@ const Register = () => {
 		<View style={{ flex: 1 }}>
 			<ScrollView contentContainerStyle={{ alignItems: "center" }}>
 				<Image source={logo} style={styles.logo} />
-				<TextInput placeholder="First Name" />
-				<TextInput placeholder="Last Name" />
-				<TextInput placeholder="Address Line 1" />
-				<TextInput placeholder="Address Line 2" />
-				<TextInput placeholder="City" />
-				<TextInput placeholder="State/Province" />
-				<TextInput placeholder="Country" />
-				<TextInput placeholder="ZIP/Postal Code" />
-				<TextInput placeholder="Username" />
-				<TextInput placeholder="Email" />
-				<TextInput placeholder="Password" />
-				<TextInput placeholder="Confirm Password" />
+				<TextInput placeholder="First Name" style={styles.input} />
+				<TextInput placeholder="Last Name" style={styles.input} />
+				<TextInput placeholder="Address Line 1" style={styles.input} />
+				<TextInput placeholder="Address Line 2" style={styles.input} />
+				<TextInput placeholder="City" style={styles.input} />
+				<TextInput placeholder="State/Province" style={styles.input} />
+				<TextInput placeholder="Country" style={styles.input} />
+				<TextInput placeholder="ZIP/Postal Code" style={styles.input} />
+				<TextInput placeholder="Username" style={styles.input} />
+				<TextInput placeholder="Email" style={styles.input} />
+				<TextInput placeholder="Password" style={styles.input} />
+				<TextInput
+					placeholder="Confirm Password"
+					style={styles.input}
+				/>
 
-				<TouchableOpacity>
-					<Text>Sign In</Text>
+				<TouchableOpacity style={styles.btn}>
+					<Text style={styles.btnTxt}>Register</Text>
 				</TouchableOpacity>
 				<Text>Already a member?</Text>
-				<Link to="/sign-in">
-					<Text>Sign In</Text>
+				<Link to="/sign-in" style={styles.btn}>
+					<Text style={styles.btnTxt}>Sign In</Text>
 				</Link>
 			</ScrollView>
 			<Footer />
@@ -53,5 +56,24 @@ const styles = StyleSheet.create({
 		marginTop: 50,
 		width: deviceWidth - 100,
 		resizeMode: "contain",
+	},
+	input: {
+		width: deviceWidth - 50,
+		borderWidth: 1,
+		marginVertical: 10,
+		borderColor: "#EFA7A1",
+		padding: 20,
+	},
+	btn: {
+		backgroundColor: "#EFA7A1",
+		width: 100,
+		height: 40,
+		alignItems: "center",
+		justifyContent: "center",
+		marginBottom: 30,
+		marginTop: 20,
+	},
+	btnTxt: {
+		color: "#fff",
 	},
 });
