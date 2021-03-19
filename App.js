1428d1e596787c9ee6b5184dce714cbf5005c376
Jaskaran Singh <jaskaran1523@gmail.com>
2021-03-19 04:19:43 -0700

    Fix push notifications

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View } from "react-native";
 import { NativeRouter, Route } from "react-router-native";
 import Challenges from "./components/Challenges/Challenges";
@@ -10,31 +10,111 @@ import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
 import Video from "./Video/Video";
 import CategoryResult from "./components/CategoryResult";
-import * as Permissions from "expo-permissions";
 import Class from "./components/Classes/Class";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
+});
 
 export default function App() {
+	const [expoPushToken, setExpoPushToken] = useState("");
+	const [notification, setNotification] = useState(false);
+	const notificationListener = useRef();
+	const responseListener = useRef();
+
 	useEffect(() => {
-		registerForPushNotifications();
+		registerForPushNotificationsAsync().then((token) =>
+			setExpoPushToken(token)
+		);
+
+		notificationListener.current = Notifications.addNotificationReceivedListener(
+			(notification) => {
+				setNotification(notification);
+			}
+		);
+
+		responseListener.current = Notifications.addNotificationResponseReceivedListener(
+			(response) => {
+				console.log(response);
+			}
+		);
+
+		sendPushNotification(expoPushToken);
+
+		return () => {
+			Notifications.removeNotificationSubscription(
+				notificationListener.current
+			);
+			Notifications.removeNotificationSubscription(
+				responseListener.current
+			);
+		};
 	}, []);
 
-	const registerForPushNotifications = async () => {
-		try {
-			const permission = await Permissions.askAsync(
-				Permissions.NOTIFICATIONS
-			);
-			if (!permission.granted) return;
-			const token = await Notifications.getExpoPushTokenAsync();
+	async function sendPushNotification(expoPushToken) {
+		const message = {
+			to: expoPushToken,
+			sound: "default",
+			title: "Pilates on Demand",
+			body: "This is a push notification",
+			data: { someData: "goes here" },
+		};
+
+		await fetch("https://exp.host/--/api/v2/push/send", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Accept-encoding": "gzip, deflate",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(message),
+		});
+	}
+
+	async function registerForPushNotificationsAsync() {
+		let token;
+		if (Constants.isDevice) {
+			const {
+				status: existingStatus,
+			} = await Notifications.getPermissionsAsync();
+			let finalStatus = existingStatus;
+			if (existingStatus !== "granted") {
+				const {
+					status,
+				} = await Notifications.requestPermissionsAsync();
+				finalStatus = status;
+			}
+			if (finalStatus !== "granted") {
+				alert("Failed to get push token for push notification!");
+				return;
+			}
+			token = (await Notifications.getExpoPushTokenAsync()).data;
 			console.log(token);
-		} catch (error) {
-			console.log("Error getting a token", error);
+		} else {
+			alert("Must use physical device for Push Notifications");
 		}
-	};
+
+		if (Platform.OS === "android") {
+			Notifications.setNotificationChannelAsync("default", {
+				name: "default",
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: "#FF231F7C",
+			});
+		}
+
+		return token;
+	}
 
 	return (
 		<NativeRouter>
 			<View style={{ flex: 1 }}>
-				{/* <Route exact path="/" component={Settings} /> */}
 				<Route exact path="/" component={Home} />
 				<Route path="/home/:loggedin/:id" component={Home} />
 				<Route path="/classes" component={Classes} />
